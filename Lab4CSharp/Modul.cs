0c2e165563443747e2bade2fc3e4c99f51242a86
Dmitry-Dem <demenchuk.dmitry03@gmail.com>
2022-11-14 10:29:35 +0200

    Complited task 1

--- Lab4CSharp/Modul.cs
+++ Lab4CSharp/Modul.cs
@@ -6,8 +6,73 @@ using System.Threading.Tasks;
 
 namespace Lab4CSharp
 {
-    internal class UserClass
+    /*----Task-1------------*/
+    class Money
     {
-        public string Name { get; set; }
+        public double First { get; set; }
+        public double Second { get; set; }
+        public double this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                {
+                    return First;
+                }
+                else if (index == 1)
+                {
+                    return Second;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+
+            set
+            {
+                if (index == 0)
+                {
+                    First = value;
+                }
+                else if (index == 1)
+                {
+                    Second = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+        public Money()
+        {
+
+        }
+        public Money(double first, double second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public static explicit operator Money(string fields) => new Money { First = Convert.ToDouble(fields.Split(" ")[0]), Second = Convert.ToDouble(fields.Split(" ")[1]) };
+
+        public static implicit operator string(Money money) => $"{money[0]} {money[1]}";
+        public static Money operator ++(Money money) => new Money { First = money[0]++, Second = money[1]++ };
+        public static Money operator --(Money money) => new Money { First = money[0]--, Second = money[1]-- };
+        public static bool operator !(Money money) => money[1] != 0;
+        public static Money operator +(Money money1, Money money2) => new Money(money1[0] + money2[0], money1[1] + money2[1]);
+        public static Money operator -(Money money1, Money money2) => new Money(money1[0] - money2[0], money1[1] - money2[1]);
     }
+    /*----------------------*/
+
+
+    /*----Task-2------------*/
+
+    /*----------------------*/
+
+
+    /*----Task-3------------*/
+
+    /*----------------------*/
 }
--- Lab4CSharp/Program.cs
+++ Lab4CSharp/Program.cs
@@ -1,60 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-/// <summary>
-///  Top-level statements 
-///  Код програми (оператори)  вищого рівня
-/// </summary>
-///
-Console.WriteLine("Lab4 C# ");
-AnyFunc();
+﻿using Lab4CSharp;
+Console.WriteLine("Lab4 C# (Demenchuk dmytro)");
 
-/// <summary>
-/// 
-///  Top-level statements must precede namespace and type declarations.
-/// At the top-level methods/functions can be defined and used
-/// На верхньому рівні можна визначати та використовувати методи/функції
-/// </summary>
-void AnyFunc()
+//task1();
+task2();
+task3();
+
+
+
+
+
+
+
+void task1()
 {
-    Console.WriteLine(" Some function in top-level");
+    Console.WriteLine("Task 1 Tests:");
+
+    Money money = (Money)"1 3";
+    string str = money;
+
+    Console.WriteLine(str);
+    Console.WriteLine(money);
+
+    Console.WriteLine(money + money++);
+    Console.WriteLine(money - ++money);
+
+    money[1] = 0;
+    Console.WriteLine(!money);
 }
-Console.WriteLine("Problems 1 ");
-AnyFunc();
-//  приклад класів
-UserClass cl = new UserClass();
-cl.Name = " UserClass top-level ";
-User.UserClass cl2 = new User.UserClass();
-cl2.Name = " UserClass namespace User ";
-Lab4CSharp.UserClass cl3 = new Lab4CSharp.UserClass();
-cl3.Name = " UserClass namespace Lab4CSharp ";
-Console.WriteLine(cl + "   " + cl2 + "  " +cl3);
-
-
-
-/// <summary>
-/// 
-/// Top-level statements must precede namespace and type declarations.
-/// Оператори верхнього рівня мають передувати оголошенням простору імен і типу.
-/// Створення класу(ів) або оголошенням простору імен є закіченням  іструкцій верхнього рівня
-/// 
-/// </summary>
-
-namespace User
+void task2()
 {
-    class UserClass
-    {
-        public string Name { get; set; }
-        UserClass()
-        {
-            Name = "NoName";
-        }
-        UserClass(string n)
-        {
-            Name = n;
-        }
-    }
-
+    Console.WriteLine("Task 2 Tests:");
 }
-class UserClass
+void task3()
 {
-    public string Name { get; set; }
+    Console.WriteLine("Task 3 Tests:");
 }
